fc26facb49530a2a775adfd1beca28defebae03e
laejmestradacabrera-tech <laejmestradacabrera@gmail.com>
2026-05-21 21:30:23 -0600

    Update app_occidente.py

--- app_occidente.py
+++ app_occidente.py
@@ -397,7 +397,20 @@ if archivo_modelos:
         top_z = df_z.sort_values(by=col_p, ascending=False).head(20).reset_index(drop=True)
         top_z.columns = ['MODELO', 'PARES VENDIDOS']
         st.table(top_z.style.apply(resaltar_top_5, axis=None))
+# --- BOTÓN DE DESCARGA DE REPORTE ---
+        txt_reporte = generar_reporte_top20_txt(
+            df_top20=top_t, 
+            nombre_sucursal=str(t_sel)
+        )
 
+        st.write("<br>", unsafe_allow_html=True)
+        st.download_button(
+            label="📄 Descargar Formato de Auditoría (Para Imprimir en Tamaño Carta)",
+            data=txt_reporte,
+            file_name=f"Auditoria_Top20_{t_sel}.txt",
+            mime="text/plain",
+            type="primary"
+        )
 # --- PESTAÑA 5: RUTA DEL CLIENTE ---
 with tab5:
     st.subheader("🧭 Protocolo Operativo en Piso de Venta")
